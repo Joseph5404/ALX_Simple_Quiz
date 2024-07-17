4660f259b1c80be2901e4ab863e88c44c6fab97f
Joseph5404 <59722056+Joseph5404@users.noreply.github.com>
2024-07-17 18:27:52 +0300

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -2,14 +2,15 @@ function checkAnswer(){
 
     const correctAnswer = "4";
 
-    const userAnswer = document.querySelector('input[name="quiz"]:checked')?.value;
+    const selectedAnswer = document.querySelector('input[name="quiz"]:checked');
+
+    const userAnswer = selectedAnswer ? selectedAnswer.value : null;
 
     const feedback = document.getElementById('feedback');
 
     if (userAnswer === null){
         feedback.textContent = "Please select an answer";
         
-
     }else if(userAnswer === correctAnswer){
             feedback.textContent = "Correct! Well done.";
             feedback.style.color = "green";
