50bd2875bf028592cd76424e6b2f6394138893dd
Joseph5404 <59722056+Joseph5404@users.noreply.github.com>
2024-07-17 17:56:00 +0300

    Add files via upload

---
+++ quiz.js
@@ -0,0 +1,24 @@
+function checkAnswer(){
+
+    const correctAnswer = "4";
+
+    const userAnswer = document.querySelector('input[name="quiz"]:checked')?.value;
+
+    const feedback = document.getElementById('feedback');
+
+    if (userAnswer){
+
+        if(userAnswer === correctAnswer){
+            feedback.textContent = "Correct! Well done.";
+            feedback.style.color = "green";
+        }else{
+            feedback.textContent = "That's incorrect. Try again";
+            feedback.style.color = "red";
+        }
+    }else{
+        feedback.textContent = "That's incorrect. Try again";
+        feedback.style.color = "orange";
+    }
+        }
+        document.getElementById('submit-answer').addEventListener('click', checkAnswer);
+    
